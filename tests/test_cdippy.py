343cf3ee2f7004ff8b4fe75c986876c7a75accff
haileyajohnson <haj022@ucsd.edu>
2025-07-02 14:20:03 -0700

    fix uninitialized variable in mop_data

--- tests/test_cdippy.py
+++ tests/test_cdippy.py
@@ -160,6 +160,7 @@ class TestMopData(unittest.TestCase):
         m = md.MopData("BP100", "ecmwf_fc")
         start = self.dt1
         d = m.get_series(start, vrs=self.v, target_records=60)
+        print(d.keys())
         self.assertEqual(len(d.keys()), 2)
         self.assertEqual(len(d["waveHs"]), 61)
 
@@ -169,6 +170,7 @@ class TestMopData(unittest.TestCase):
         m = md.MopData("BP100", "forecast")
         start = self.dt1
         d = m.get_series(start, vrs=self.v, target_records=60)
+        print(d.keys())
         self.assertEqual(len(d.keys()), 2)
         self.assertEqual(len(d["waveHs"]), 61)
 
